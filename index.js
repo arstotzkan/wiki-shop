4d441d88433e7a630194131f5b14b9ebcaf3d220
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2022-12-28 16:30:39 +0200

    add urls for login/logout

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require("body-parser");
+const contacts = require("./models/contacts.js");
+const USER_CONTROLLER = contacts.USER_CONTROLLER;
+
 const app = express()
 const port = 8080
 
@@ -35,7 +39,6 @@ app.get('/categories', function(req, res){
     var options = {
         root: path.join(__dirname, 'public', 'templates')
     }
-    console.log(__dirname);
 
     res.sendFile('categories.html', options, function(err){
         
@@ -54,6 +57,54 @@ app.get('/cart', function(req, res){
         console.log(err)
     })
 })
+
+app.get("/signup" ,function(req,res){
+    var options = {
+        root: path.join(__dirname, 'public', 'templates')
+    }
+
+    res.sendFile('signup.html', options, function(err){
+        console.log(err);
+    })
+})
+
+app.post("/create-account", function(req, res) {
+    let username = req.body.username;
+    let pwd = req.body.password;
+
+    if (USER_CONTROLLER.getUserFromUsername(username)){
+        res.redirect("/signup")
+    }
+    else{
+       USER_CONTROLLER.addUser(username, pwd)
+       res.redirect("/") //res.redirect("back")
+    }
+})
+
+app.get("/login" ,function(req,res){
+    var options = {
+        root: path.join(__dirname, 'public', 'templates')
+    }
+
+    res.sendFile('login.html', options, function(err){
+        console.log(err);
+    })
+})
+
+app.post("/check-login", function(req, res) {
+    let username = req.body.username;
+    let pwd = req.body.password;
+
+    let userDatum = USER_CONTROLLER.getUserFromUsername(username)
+
+    if (userDatum && USER_CONTROLLER.userDataIsCorrect(userDatum, username, pwd) ){
+        res.redirect("/")
+    }
+    else{
+       res.redirect("/login")//res.redirect("back")
+    }
+})
+
 
 //FOR AJAX
 app.get('/site/header', function(req, res){
